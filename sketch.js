13a603fd7bbc8f4201fe54c2e166275486ae8c5a
KingPig2910 <73672641+KingPig2910@users.noreply.github.com>
2021-01-24 19:13:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var dog, dogImage, happyDog, happyDogImage;
 var database;
 var milk, milkImage;
 var foodStock;
-var food;
+var foodS;
 
 function preload(){
   dogImage = loadImage("images/dogImg.png");
@@ -22,8 +22,6 @@ function setup() {
   milk = createSprite(200,300,50,50);
   milk.addImage(milkImage);
   milk.scale = 0.1;
-
- food = 19;
   
   
 }
@@ -31,25 +29,18 @@ function setup() {
 
 function draw() {  
 background("green");
-if(keyDown(UP_ARROW)){
-  food = food - 1;
+if(keyWentDown(UP_ARROW)){
+  writeStock(foodS);
   dog.addImage(happyDogImage); 
-  milk.visible = false;
-}else{
-  dog.addImage(dogImage);
-  milk.visible = true;
-}
-if(food === 0){
-  milk.visible = false;
 }
 foodStock = database.ref('Food');
-  foodStock.on("value", readStock);
+foodStock.on("value",readStock);
 
 textSize(20);
 text("Note: Press UP_ARROW Key To Feed Doggo Milk!", 40, 70);
 
 textSize(20);
-text("Food:19", 40, 90);
+text("Food Remaining:"+ foodS, 40, 90);
 
 
   drawSprites();
